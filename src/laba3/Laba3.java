75e0fc5631aa28bde9ffb12419bc6a97bcd05b73
Александр Сергеев <alexandrsergeev071101@yandex.ru>
2021-12-03 10:27:07 +0300

    rename

--- src/laba3/Laba3.java
+++ src/laba3/Laba3.java
@@ -6,10 +6,10 @@ import java.util.regex.Pattern;
 public class Laba3 {
     public static void main(String[] args) {
         String text = "255.255.255.0";
-        System.out.println(preOrPri(text));
+        System.out.println(checkApi(text));
     }
 
-    public static boolean preOrPri(String text) {
+    public static boolean checkApi(String text) {
         return text.matches("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
     }
 }
